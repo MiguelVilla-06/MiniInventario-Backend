c61e8c3906ecd1333dffefc2a7891452a5507a82
Miguel Villanueva <miguel.villanova15@gmail.com>
2026-06-24 10:42:55 -0600

    Primer commit Backend

--- src/main/java/com/ipn/mx/miniinventario/features/categoria/controller/CategoriaController.java
+++ src/main/java/com/ipn/mx/miniinventario/features/categoria/controller/CategoriaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/categorias")
+@RequestMapping("/api/v1/Categoria")
 @CrossOrigin(origins = "*")
 public class CategoriaController {
     @Autowired
--- src/main/java/com/ipn/mx/miniinventario/features/producto/controller/ProductoController.java
+++ src/main/java/com/ipn/mx/miniinventario/features/producto/controller/ProductoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/productos")
+@RequestMapping("/api/v1/Producto")
 @CrossOrigin(origins = "*")
 public class ProductoController {
     @Autowired
